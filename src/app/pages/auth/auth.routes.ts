52d7fd2ee508acfeb0cd3cf5ac9551188e3ceb6e
arquitectosoftware1 <arquitectosoftware1_lem@castores.com.mx>
2026-07-11 13:41:54 -0600

    fix: se corrigen errores de eslint

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { Login } from './login';
 
-export default [
-    { path: 'login', component: Login }
-] as Routes;
+export default [{ path: 'login', component: Login }] as Routes;
