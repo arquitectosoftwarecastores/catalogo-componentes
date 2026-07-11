6111a20f8994ddf2997e3a2b623932e7a4ac7193
arquitectosoftware1 <arquitectosoftware1_lem@castores.com.mx>
2026-07-11 13:35:45 -0600

    refactor: se eliminan recursos no usados

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { Documentation } from './documentation/documentation';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
 
 export default [
-    { path: 'documentation', component: Documentation },
     { path: 'crud', data: { breadcrumb: 'Crud' }, component: Crud },
     { path: 'empty', data: { breadcrumb: 'Empty' }, component: Empty },
     { path: '**', redirectTo: '/notfound' }
